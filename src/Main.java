ac5fd7dd1a983fc1849a658065728586cebedbe1
pdefraene <pierredefraene@gmail.com>
2020-10-28 14:45:20 +0100

    need a file

--- src/Main.java
+++ src/Main.java
@@ -14,8 +14,13 @@ public class Main {
     */
     public static void main(String[] args) {
 
+      if (args.length == 0){
+        System.out.println("need a .fs file for running");
+        System.exit(0);
+      }
       Map<Object, Integer> variables = new HashMap<Object, Integer>();
       ArrayList<Symbol> symbolList = new ArrayList<Symbol>();
+
 
         java.io.FileInputStream stream = null;
         try {
